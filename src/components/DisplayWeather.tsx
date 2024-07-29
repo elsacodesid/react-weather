88ad2a483ac6f951259494c8b4d1f226a4da7d54
elsacodesid <elsacodesid@gmail.com>
2024-07-29 09:00:02 +0700

    update

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -26,12 +26,14 @@ type WeatherData = {
   };
 };
 
-
 const DisplayWeather = () => {
   const api_key = "2ad64a080d7c1ba89ad0aaa9dda2c1e4";
   const api_endpoint = "https://api.openweathermap.org/data/2.5/weather?";
 
-  const [currentWeather, setCurrentWeather] = useState<WeatherData | null>(null);
+  const [currentWeather, setCurrentWeather] = useState<WeatherData | null>(
+    null
+  );
+  const [userCity, setUserCity] = useState("");
 
   const fetchData = async (lat: number, lon: number) => {
     const url = `${api_endpoint}lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
@@ -44,53 +46,97 @@ const DisplayWeather = () => {
       const { latitude, longitude } = position.coords;
       fetchData(latitude, longitude).then((weather) => {
         if (weather) {
-          console.log(weather)
-          console.log("name: ", weather.name)
-          setCurrentWeather(weather)
+          console.log(weather);
+          console.log("name: ", weather.name);
+          setCurrentWeather(weather);
         }
       });
     });
   }, []);
-  
+
+  const fetchUserCity = async (userCity: string) => {
+    const url = `${api_endpoint}q=${userCity}&appid=${api_key}&units=metric`;
+    const res = await axios.get(url);
+    return res.data;
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") {
+      handleSearch();
+    }
+  };
+
+  const handleSearch = async () => {
+    if (userCity) {
+      try {
+        const weather = await fetchUserCity(userCity);
+        setCurrentWeather(weather);
+      } catch (error) {
+        console.error("Error fetching city weather:", error);
+      }
+    }
+  };
+
   return (
     <MainWrapper>
-    <div className="container">
-      <div className="searchArea">
-        <input type="text" placeholder="Enter a city..." />
-        <div className="searchCircle">
-          <FcSearch className="searchIcon" />
-        </div>
-      </div>
-      <div className="weatherArea">
-      <h1>{currentWeather ? currentWeather.name : "Loading..."}</h1>
-         <span>{currentWeather ? currentWeather.sys.country : ""}</span>
-        <div className="icon">
-          <IoSunny color="yellow" />
-          <IoPartlySunnyOutline color="orange" />
-          <FaCloudShowersHeavy color="blue" />
-          <BsCloudHaze color="#B0BEC5" />
-        </div>
-        <h1>{currentWeather?.main ? currentWeather.main.temp : "Loading..."}°C</h1>
-        <h2>{currentWeather?.weather[0] ? currentWeather?.weather[0]?.description : "Loading..."}</h2>
-      </div>
-      <div className="bottomInfoArea">
-        <div className="humidityLevel">
-          <WiHumidity className="humidIcon" />
-          <div className="humidInfo">
-            <h1>{currentWeather?.main ? currentWeather.main.humidity : "Loading..."}%</h1>
-            <p>Humidity</p>
+      <div className="container">
+        <div className="searchArea">
+          <input
+            type="text"
+            placeholder="Enter a city..."
+            onChange={(e) => setUserCity(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <div className="searchCircle">
+            <FcSearch className="searchIcon" onClick={handleSearch} />
           </div>
         </div>
-        <div className="wind">
-          <FaWind className="windIcon" />
-          <div className="humidInfo">
-            <h1>{currentWeather?.wind ? currentWeather.wind.speed : "Loading..." }km/h</h1>
-            <p>Wind speed</p>
+        <div className="weatherArea">
+          <h1 className="cityName">{currentWeather ? currentWeather.name : "Loading..."}</h1>
+          <span>{currentWeather ? currentWeather.sys.country : ""}</span>
+          <div className="icon">
+            <IoSunny color="yellow" />
+            <IoPartlySunnyOutline color="orange" />
+            <FaCloudShowersHeavy color="blue" />
+            <BsCloudHaze color="#B0BEC5" />
+          </div>
+          <h1>
+            {currentWeather?.main ? currentWeather.main.temp : "Loading..."}°C
+          </h1>
+          <h2>
+            {currentWeather?.weather[0]
+              ? currentWeather?.weather[0]?.description
+              : "Loading..."}
+          </h2>
+        </div>
+        <div className="bottomInfoArea">
+          <div className="humidityLevel">
+            <WiHumidity className="humidIcon" />
+            <div className="humidInfo">
+              <h1>
+                {currentWeather?.main
+                  ? currentWeather.main.humidity
+                  : "Loading..."}
+                %
+              </h1>
+              <p>Humidity</p>
+            </div>
+          </div>
+          <div className="wind">
+            <FaWind className="windIcon" />
+            <div className="humidInfo">
+              <h1>
+                {currentWeather?.wind
+                  ? currentWeather.wind.speed
+                  : "Loading..."}
+                km/h
+              </h1>
+              <p>Wind speed</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </MainWrapper>
+    </MainWrapper>
   );
 };
 
--- src/components/weather.module.ts
+++ src/components/weather.module.ts
@@ -1,10 +1,62 @@
 import styled from "styled-components";
 
 export const MainWrapper = styled.div`
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center
- 
+ display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(135deg, #71b7e6, #9b59b6);
+
+.container{
+  background: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+  width: 350px;
+  text-align: center;
+}
+
+.searchArea {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.searchInput = styled.input{
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  width: 70%;
+}
+
+.searchCircle {
+  background: #71b7e6;
+  border-radius: 50%;
+  padding: 10px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.weatherArea {
+  margin-bottom: 20px;
+}
+
+.cityName 
+{  font-size: 2.5em;
+  margin: 0;}
+
+
+.country 
+{  font-size: 1.2em;
+  color: #999;}
+
+
+.icon 
+{  display: flex;
+  justify-content: center;
+  margin: 20px 0;}
    
 `;
